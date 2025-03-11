44c13f62c9abf063d1f5f203b42f485e95921f23
Naxalin <gonzialconerofdez@gmail.com>
2025-03-11 15:53:02 +0100

    Add App and DefaultLayout components with router and layout structure

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Menu from '@/components/Menu.vue';
+import Footer from '@/components/Footer.vue';
+</script>
+
+<template>
+    <div class="container">
+        <header>
+            <Menu />
+        </header>
+        <main>
+            <slot></slot>
+        </main>
+
+        <Footer />
+
+    </div>
+</template>
+
+<style scoped></style>
