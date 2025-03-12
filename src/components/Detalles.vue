91e9df9fadf3755bca9c6e373c74807c12376e27
Naxalin <gonzialconerofdez@gmail.com>
2025-03-12 15:51:50 +0100

    Add Detalles component and create DetallesView with DefaultLayout integration

---
+++ src/components/Detalles.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+    <section class="grid grid-cols-2 bg-amber-600">
+        <div class="bg-red-300">
+
+        </div>
+        <div class="bg-green-400">
+
+        </div>
+    </section>
+
+</template>
+
+<style scoped></style>
---
+++ src/views/DetallesView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Detalles from '@/components/Detalles.vue'
+import DefaultLayout from '@/layouts/DefaultLayout.vue';
+</script>
+<template>
+    <DefaultLayout>
+        <Detalles class="" />
+    </DefaultLayout>
+</template>
