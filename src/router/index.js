d40153a009cefbc949ebc06d2a6710794d8b63d2
Naxalin <gonzialconerofdez@gmail.com>
2025-03-12 15:52:00 +0100

    Add Detalles route to the router configuration

--- src/router/index.js
+++ src/router/index.js
@@ -1,40 +1,38 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
 import EstrenosView from '@/views/EstrenosView.vue'
 import CartaPeliculasView from '@/views/CatalogoView.vue'
 import SesionView from '@/views/SesionView.vue'
+import detalles from '@/views/DetallesView.vue'
 import Main from '@/components/Main.vue'
 const router = createRouter({
-  
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView,
+      path: '/detalles',
+      name: 'detalles',
+      component: detalles,
     },
     {
-      path:'/estrenos',
-      name:'estrenos',
+      path: '/estrenos',
+      name: 'estrenos',
       component: EstrenosView,
     },
     {
-      path:'/sesion',
-      name:'sesion',
+      path: '/sesion',
+      name: 'sesion',
       component: SesionView,
     },
     {
-      path:'/catalogo',
-      name:'catalogo',
+      path: '/catalogo',
+      name: 'catalogo',
       component: CartaPeliculasView,
     },
     {
-      path:'/inicio',
-      name:'principio',
-      component: Main
+      path: '/inicio',
+      name: 'principio',
+      component: Main,
     },
   ],
 })
-
 
 export default router
