319d3009dec64b4a1588caf3a1f4142784d57b90
Naxalin <gonzialconerofdez@gmail.com>
2025-03-12 12:19:15 +0100

    Add Fondo image asset and import Flowbite in main.js

--- src/main.js
+++ src/main.js
@@ -3,11 +3,11 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import DefaultLayout from './layouts/DefaultLayout.vue'  
+import DefaultLayout from './layouts/DefaultLayout.vue'
+import 'flowbite'
 
 const app = createApp(App)
 
 app.use(router)
-app.component('DefaultLayout', DefaultLayout);  
+app.component('DefaultLayout', DefaultLayout)
 app.mount('#app')
-
