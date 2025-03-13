9dad3246cddaf406b2adbccb5763029c317fe94d
Naxalin <gonzialconerofdez@gmail.com>
2025-03-13 14:46:52 +0100

    Add new routes for Registro, DerechosAutor, Cookies, and PoliticasPrivacidad views; update Detalles route to include ID parameter

--- src/router/index.js
+++ src/router/index.js
@@ -5,18 +5,42 @@ import SesionView from '@/views/SesionView.vue'
 import detalles from '@/views/DetallesView.vue'
 import Main from '@/views/InicioView.vue'
 import UltimasPeliculasView from '@/views/UltimasPeliculasView.vue'
+import RegistroView from '@/views/RegistroView.vue'
+import DerechosAutor from '@/views/DerechosAutorView.vue'
+import CookiesView from '@/views/CookiesView.vue'
+import PoliticasPrivacidadView from '@/views/PoliticasPrivacidadView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/detalles',
+      path: '/detalles/:id',
       name: 'detalles',
       component: detalles,
+    },
+    {
+      path: '/derechos-de-autor',
+      name: 'derechos',
+      component: DerechosAutor,
+    },
+    {
+      path: '/cookies',
+      name: 'cookies',
+      component: CookiesView,
+    },
+    {
+      path: '/politicas-de-privacidad',
+      name: 'politicas',
+      component: PoliticasPrivacidadView,
     },
     {
       path: '/ultimas',
       name: 'ultimas',
       component: UltimasPeliculasView,
+    },
+    {
+      path: '/registro',
+      name: 'registro',
+      component: RegistroView,
     },
     {
       path: '/estrenos',
